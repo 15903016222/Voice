3c9ebb9941d052639373088ca91464742a5a4813
Jake Yang <yanghuanjie@cndoppler.cn>
2016-08-15 15:12:46 +0800

    Update: mcu.h

--- mcu/mcu.h
+++ mcu/mcu.h
@@ -1,35 +1,63 @@
-#include "mcu.h"
+#ifndef MCU_H
+#define MCU_H
 
-Mcu *Mcu::m_mcu = NULL;
-const char Mcu::m_queryTempData[6]={0x24, 0x24, 0x52, 0x1, 0x23, 0x23};
-const char Mcu::m_queryBatteryData[6]={0x24, 0x24, 0x52, 0x3, 0x23, 0x23};
-const char Mcu::m_queryBrightnessData[6]={0x24, 0x24, 0x52, 0x6, 0x23, 0x23};
-char Mcu::m_setBrightnessData[7]={0x24, 0x24, 0x53, 0x6, 0x0, 0x23, 0x23};
+#include <QtSerialPort>
 
-Mcu::Mcu()
-    :QSerialPort("/dev/ttyMUX2")
+class Mcu : public QSerialPort
 {
-    connect(this, &Mcu::readyRead, this, &Mcu::on_readyRead_event);
-    open(QIODevice::ReadWrite);
-    setBaudRate(Baud115200);
-    setDataBits(Data8);
-    setFlowControl(NoFlowControl);
-    setStopBits(OneStop);
-    setParity(OddParity);
+    Q_OBJECT
+public:
+    static Mcu* get_mcu();
+    static void destroyed();
+
+    void query_temp() { write(m_queryTempData, sizeof(m_queryTempData)); }
+    void query_battery() { write(m_queryBatteryData, sizeof(m_queryBatteryData)); }
+    void query_brightness() { write(m_queryBrightnessData, sizeof(m_queryBrightnessData)); }
+    void set_brightness(uint8_t light) { m_setBrightnessData[4]=light; write(m_setBrightnessData, sizeof(m_setBrightnessData)); }
+
+    enum EventType {
+        TEMPERATURE,
+        BATTERY,
+        BRIGHTNESS,
+        POWEROFF,
+        KEY,
+    };
+
+Q_SIGNALS:
+    void event(EventType type, int val);
+
+protected:
+    Mcu();
+    ~Mcu();
+
+private:
+    static Mcu *m_mcu;
+    QMutex wrMutex;
+
+    static const char m_queryTempData[6];
+    static const char m_queryBatteryData[6];
+    static const char m_queryBrightnessData[6];
+    static char m_setBrightnessData[7];
+
+    inline qint64 write(const char *data, qint64 len) { QMutexLocker locker(&wrMutex); return QIODevice::write(data, len); }
+    void on_readyRead_event();
+};
+
+
+inline Mcu* Mcu::get_mcu()
+{
+    if (m_mcu == NULL) {
+        m_mcu = new Mcu();
+    }
+    return m_mcu;
 }
 
-Mcu::~Mcu()
+inline void Mcu::destroyed()
 {
-    close();
+    if (m_mcu) {
+        delete m_mcu;
+        m_mcu = NULL;
+    }
 }
 
-void Mcu::on_readyRead_event()
-{
-    EventType type;
-    int val = 0;
-    QByteArray byte = readAll();
-    /*开始解析数据*/
-
-    /*发送信号*/
-    emit event(type, val);
-}
+#endif // MCU_H
