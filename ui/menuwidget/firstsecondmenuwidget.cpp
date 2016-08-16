2fd8f6ff61bce31b86a7089e88e293681770fe13
ZJY <455742444@qq.com>
2016-08-16 16:10:21 +0800

    修改：更改QStandradModel的定义方式，ListView不增加空内容

--- ui/menuwidget/firstsecondmenuwidget.cpp
+++ ui/menuwidget/firstsecondmenuwidget.cpp
@@ -9,7 +9,10 @@ FirstSecondMenuWidget::FirstSecondMenuWidget(QWidget *parent) :
 
   initUI();
 
-  hideFirstMenuPage();
+  QModelIndex initModelIndex = modelList.at(0)->index(0, 0);
+  menuList.at(0)->setCurrentIndex(initModelIndex);
+
+//  hideFirstMenuPage();
 
 }
 
@@ -34,44 +37,31 @@ void FirstSecondMenuWidget::initUI()
   for(int i = 0; i < FIRST_MENU_NUMBER; i++)
   {
     QListView* listView = findChild<QListView*>("listView_" + QString::number(i+1));
+    listView->setStyleSheet("QListView{font: 13px}");
     menuList.append(listView);
 
-    QStandardItemModel *standardItemModel_1 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_2 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_3 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_4 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_5 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_6 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_7 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_8 = new QStandardItemModel(this);
-    QStandardItemModel *standardItemModel_9 = new QStandardItemModel(this);
-
-    modelList.append(standardItemModel_1);
-    modelList.append(standardItemModel_2);
-    modelList.append(standardItemModel_3);
-    modelList.append(standardItemModel_4);
-    modelList.append(standardItemModel_5);
-    modelList.append(standardItemModel_6);
-    modelList.append(standardItemModel_7);
-    modelList.append(standardItemModel_8);
-    modelList.append(standardItemModel_9);
-
+    QStandardItemModel *standardItemModel = new QStandardItemModel(this);
+    standardItemModel->setObjectName("standardItemModel_"+QString::number(i+1));
+    modelList.append(standardItemModel);
 
     QStringList secondMenuList;
     for(int j = 0; j < SECOND_MENU_NUMBER; j++)
     {
-      secondMenuList.append(SECOND_MENU_STRING[i][j]);
+      if(SECOND_MENU_STRING[i][j] != NULL)
+      {
+        secondMenuList.append(SECOND_MENU_STRING[i][j]);
+        QString string = static_cast<QString>(secondMenuList.at(j));
+        QStandardItem *item = new QStandardItem(string);
+        modelList.at(i)->appendRow(item);
+        item->setForeground(QBrush(Qt::yellow));
 
-      QString string = static_cast<QString>(secondMenuList.at(j));
-      QStandardItem *item = new QStandardItem(string);
-      modelList.at(i)->appendRow(item);
-      item->setForeground(QBrush(Qt::yellow));
+      }
     }
 
     QModelIndex initModelIndex = modelList.at(i)->index(0, 0);
     QStandardItem *initItem = modelList.at(i)->itemFromIndex(initModelIndex);
     initItem->setForeground(QBrush(Qt::red));
-
+    menuList.at(i)->setCurrentIndex(initModelIndex);
     menuList.at(i)->setModel(modelList.at(i));
   }
 }
