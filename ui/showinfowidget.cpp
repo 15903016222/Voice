78f219904dc223682b7dfc530b65b38b61b18e15
Shaoyi Yan <yanshaoyi@sw.cndoppler.cn>
2016-08-16 14:30:43 +0800

    Add: ShowInfoWidget class

---
+++ ui/showinfowidget.cpp
@@ -0,0 +1,40 @@
+#include "showinfowidget.h"
+#include "ui_showinfowidget.h"
+
+#include <QTimer>
+#include <QDateTime>
+
+ShowInfoWidget::ShowInfoWidget(QWidget *parent) :
+  QWidget(parent),
+  ui(new Ui::ShowInfoWidget)
+{
+  ui->setupUi(this);
+
+  initShowTime();
+}
+
+ShowInfoWidget::~ShowInfoWidget()
+{
+  delete ui;
+}
+
+
+void ShowInfoWidget::displayTime()
+{
+  QDateTime time = QDateTime::currentDateTime();
+  QString str_time = time.toString("yyyy-MM-dd hh:mm:ss");
+  ui->label_5_showDateTime->setText(str_time);
+}
+
+void ShowInfoWidget::initShowTime()
+{
+  displayTime();
+  QTimer *timer = new QTimer(this);
+  connect(timer, SIGNAL(timeout()), this, SLOT(slotUpdateTime()));
+  timer->start(500);
+}
+
+void ShowInfoWidget::slotUpdateTime()
+{
+  displayTime();
+}
---
+++ ui/showinfowidget.h
@@ -0,0 +1,32 @@
+#ifndef SHOWINFOWIDGET_H
+#define SHOWINFOWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+  class ShowInfoWidget;
+}
+
+class ShowInfoWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit ShowInfoWidget(QWidget *parent = 0);
+  ~ShowInfoWidget();
+
+private:
+  Ui::ShowInfoWidget *ui;
+
+public:
+  void displayTime();
+  void initShowTime();
+
+signals:
+
+public slots:
+  void slotUpdateTime();
+
+};
+
+#endif // SHOWINFOWIDGET_H
