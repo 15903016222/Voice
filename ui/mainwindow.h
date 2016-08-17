e00990f9098e5abe0afd481e9aaa87081f902f37
ZJY <455742444@qq.com>
2016-08-17 16:14:22 +0800

    增加：点击按钮可使左侧主菜单上下滚动；左侧主菜单和常用菜单可随窗口伸缩变化

--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -6,6 +6,7 @@
 
 #include <QMainWindow>
 #include <QGst/Message>
+#include <QScrollArea>
 
 namespace Ui {
 class MainWindow;
@@ -31,7 +32,11 @@ private:
 
 public:
   CommonMenuWidget * commonMenuWidget;
+  FirstSecondMenuWidget *firstSecondMenu;
+
   void linkPluginsToConnectDevice();
+  void resizeEvent(QResizeEvent *event);
+  void arrowShowFlag();
   bool eventFilter(QObject *object, QEvent *event);
   bool hiddenSecondMenuFlag;
   bool hiddenThirdMenuFlag;
@@ -48,6 +53,8 @@ private slots:
   void slot_setThirdMenuName(int index);
   void slot_secondMenuItemClicked(QModelIndex index);
   void onGstBusMessage(const QGst::MessagePtr & message);
+  void slot_pushButton_top_Clicked();
+  void slot_pushButton_bottom_Clicked();
 
 };
 
