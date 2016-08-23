d37b9b40de0426a9467b7d5534e84044e7adf7ae
ZJY <455742444@qq.com>
2016-08-23 13:54:33 +0800

    修改：更改三级菜单表格样式

--- ui/menuwidget/thirdmenuwidget.cpp
+++ ui/menuwidget/thirdmenuwidget.cpp
@@ -31,15 +31,23 @@ void ThirdMenuWidget::initStandardModel()
 
   ui->tableView->horizontalHeader()->setDefaultAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
   ui->tableView->horizontalHeader()->setStyleSheet("QHeaderView::section"
-                                                   "{font: 11pt 'Times New Roman';background-color:rgba(61, 61, 61, 0.5);"
-                                                   "color:rgba(255, 255, 255, 255); z}");
+                                                   "{font: 11pt 'Times New Roman';"
+                                                   "background-color: rgba(0, 130, 195, 255);"
+                                                   "color: rgba(255, 255, 255, 255);"
+                                                   "border: 0px solid black;"
+                                                   "border-left:1px solid qlineargradient(spread:reflect, x1:0.49435, y1:0.068, x2:0.50565, y2:0.75, stop:0.158192 rgba(0, 130, 195, 255), stop:0.559322 rgba(255, 255, 255, 255));"
+                                                   "border-right:1px solid qlineargradient(spread:reflect, x1:0.5, y1:0.028, x2:0.5, y2:1, stop:0.158192 rgba(0, 130, 195, 255), stop:0.559322 rgba(0, 0, 0, 255));}");
+
+//  ui->tableView->setStyleSheet("QTableView::item{"
+//                               "border-left:1px solid qlineargradient(spread:pad, x1:0.5, y1:0.15, x2:0.5, y2:1, stop:0.158192 rgba(255, 255, 255, 255), stop:0.757062 rgba(0, 130, 195, 255));"
+//                               "border-right:1px solid qlineargradient(spread:pad, x1:0.5, y1:0.15, x2:0.5, y2:1, stop:0.158192 rgba(0, 0, 0, 255), stop:0.757062 rgba(0, 130, 195, 255));}");
+
 
   for(int k = 0; k < THIRD_MENU_NUMBER; k++)
   {
     QModelIndex index = model->index(k, 0, QModelIndex());
     model->setData(index, k);
   }
-
   ui->tableView->show();
 }
 
@@ -54,6 +62,9 @@ void ThirdMenuWidget::setThirdMenuName(int i, int j)
     if(THIRD_MENU_STRING[i][j][k] != NULL)
     {
       widgetStyleChoice(i, j, k);
+      model->item(0, k)->setTextAlignment(Qt::AlignCenter);
+      model->item(0, k)->setForeground(Qt::yellow);
+      model->item(0, k)->setFont(QFont("Times New Roman", 11));
     }
     else
     {
@@ -69,6 +80,7 @@ void ThirdMenuWidget::setThirdMenuName(int i, int j)
 void ThirdMenuWidget::widgetStyleChoice(int i, int j, int k)
 {
   model->setHeaderData(k, Qt::Horizontal, THIRD_MENU_STRING[i][j][k]);
+
   switch(WIDGET_CHOICE_CHAR[i][j][k].toInt())
   {
   case 1:
@@ -77,9 +89,6 @@ void ThirdMenuWidget::widgetStyleChoice(int i, int j, int k)
       model->horizontalHeaderItem(k)->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
       QStandardItem *item = new QStandardItem(QString::number(0, 'f', 2));
       model->setItem(0, k, item);
-      model->item(0, k)->setTextAlignment(Qt::AlignCenter);
-      model->item(0, k)->setForeground(Qt::yellow);
-      model->item(0, k)->setFont(QFont("Times New Roman", 11));
       ui->tableView->setItemDelegateForColumn(k, doubleSpinBox);
       ui->tableView->setEditTriggers(QAbstractItemView::CurrentChanged);
       break;
@@ -90,20 +99,14 @@ void ThirdMenuWidget::widgetStyleChoice(int i, int j, int k)
       model->horizontalHeaderItem(k)->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
       QStandardItem *item = new QStandardItem(QString("off"));
       model->setItem(0, k, item);
-      model->item(0, k)->setTextAlignment(Qt::AlignCenter);
-      model->item(0, k)->setForeground(Qt::yellow);
-      model->item(0, k)->setFont(QFont("Times New Roman", 11));
       ui->tableView->setItemDelegateForColumn(k, comboBox);
       break;
     }
   case 3:
     {
       model->horizontalHeaderItem(k)->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-      QStandardItem *item = new QStandardItem(QString("on"));
+      QStandardItem *item = new QStandardItem(QString(""));
       model->setItem(0, k, item);
-      model->item(0, k)->setTextAlignment(Qt::AlignCenter);
-      model->item(0, k)->setFont(QFont("Times New Roman", 11));
-      model->item(0, k)->setFlags(Qt::NoItemFlags);
       break;
     }
   default:
