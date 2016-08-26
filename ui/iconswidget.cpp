dcfe8868313477e91dfef8742ec67d64f36ac324
Shaoyi Yan <yanshaoyi@sw.cndoppler.cn>
2016-08-26 15:05:14 +0800

    增加：增加电量图标显示代码（待验证）

--- ui/iconswidget.cpp
+++ ui/iconswidget.cpp
@@ -27,3 +27,36 @@ void IconsWidget::on_pushButton_keyboard_clicked()
   myInputPanelDlg.showNormal();
   myInputPanelDlg.exec();
 }
+
+void IconsWidget::showBattery()
+{
+  SYSTEM_POWER_STATUS systemPowerSatus;
+  GetSystemPowerStatus(&systemPowerSatus);
+  int remaindPower = (int)systemPowerSatus.BatteryLifePercent;
+
+  if(remaindPower > 75)
+  {
+    QPixmap pixmapBattery(":/file/resources/battery2.jpg");
+    ui->label_2->setPixmap(pixmapBattery);
+  }
+  else if(remaindPower > 50 && remaindPower <= 75)
+  {
+    QPixmap pixmapBattery(":/file/resources/BatteryIcon2.png");
+    ui->label_1->setPixmap(pixmapBattery);
+  }
+  else if(remaindPower > 25 && remaindPower <= 50)
+  {
+    QPixmap pixmapBattery(":/file/resources/BatteryIcon1.png");
+    ui->label_1->setPixmap(pixmapBattery);
+  }
+  else if(remaindPower > 0 && remaindPower <= 25)
+  {
+    QPixmap pixmapBattery(":/file/resources/BatteryIcon1.png");
+    ui->label_1->setPixmap(pixmapBattery);
+  }
+  else
+  {
+    QPixmap pixmapBattery(":/file/resources/BatteryIcon1.png");
+    ui->label_2->setPixmap(pixmapBattery);
+  }
+}
--- ui/iconswidget.h
+++ ui/iconswidget.h
@@ -22,6 +22,7 @@ private:
 
 public:
   void iconsWidgetRetranslateUi();
+  void showBattery();
 
 signals:
 
