fb1f139a457baf7f9ebed01716f1af1ab9f79936
Shaoyi Yan <yanshaoyi@sw.cndoppler.cn>
2016-11-08 17:00:14 +0800

    Added: 重新spinBox类，使DateTimeSetDialog和NetWorkDialog中的SpinBox控件可以通过飞梭键和确定键改变其值；

---
+++ ui/spinbox/spinbox.cpp
@@ -0,0 +1,38 @@
+#include "spinbox.h"
+
+#include <QEvent>
+#include <QKeyEvent>
+#include <QApplication>
+
+SpinBox::SpinBox(QWidget *parent) :
+    QSpinBox(parent)
+{
+    m_mcu = Mcu::get_mcu();
+    connect(m_mcu, SIGNAL(rotary_event(Mcu::RotaryType)), this, SLOT(do_rotary_event(Mcu::RotaryType)));
+    connect(m_mcu, SIGNAL(key_event(int)), this, SLOT(key_sure(int)));
+}
+
+void SpinBox::do_rotary_event(Mcu::RotaryType type)
+{
+    if(hasFocus()){
+        if (type == Mcu::ROTARY_UP) {
+            this->stepUp();
+        } else {
+            this->stepDown();
+        }
+    }
+}
+
+void SpinBox::key_sure(int key)
+{
+    if(hasFocus()){
+        if(key == 214) {
+            QKeyEvent *event = new QKeyEvent(QEvent::KeyPress, Qt::Key_Return, Qt::NoModifier);
+            QApplication::sendEvent(this, event);
+        }
+    }
+}
+
+SpinBox::~SpinBox()
+{
+}
