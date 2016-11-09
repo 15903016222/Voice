fbfe56776c01b6ebfc7aa8df3cc41c786516a975
Jake Yang <yanghuanjie@cndoppler.cn>
2016-11-09 14:12:20 +0800

    Updated: Device for pc

--- device/device_pc.cpp
+++ device/device_pc.cpp
@@ -9,3 +9,104 @@
 
 #include "device.h"
 
+class DevicePrivate
+{
+public:
+    DevicePrivate();
+    QString m_serialNo;
+    QString m_version;
+    QString m_typeString;
+    Device::Type m_type;
+};
+DevicePrivate::DevicePrivate()
+{
+    m_serialNo = "";
+    m_version = "";
+#ifdef PC_UNIX
+    m_type = Device::DEV_PC_UNIX;
+    m_typeString = "Unix PC";
+#elif PC_WIN
+    m_type = Device::DEV_PC_WIN;
+    m_typeString = "Window Pc";
+#endif
+}
+
+/********************* Device ***************************/
+Device *Device::s_device = NULL;
+QMutex Device::s_mutex;
+
+Device *Device::get_device()
+{
+    QMutexLocker l(&s_mutex);
+    if (s_device == NULL) {
+        s_device = new Device();
+    }
+    return s_device;
+}
+
+const QString &Device::version() const
+{
+    return d->m_version;
+}
+
+Device::Type Device::type() const
+{
+    return d->m_type;
+}
+
+const QString &Device::type_string() const
+{
+    return d->m_typeString;
+}
+
+const QString &Device::serial_number() const
+{
+    return d->m_serialNo;
+}
+
+uint Device::date_time() const
+{
+    return ::time(NULL);
+}
+
+bool Device::set_date_time(uint t)
+{
+    Q_UNUSED(t);
+    return true;
+}
+
+uint Device::run_time() const
+{
+    return 0;
+}
+
+int Device::run_count() const
+{
+    return 0;
+}
+
+bool Device::import_cert(const QString &certFile)
+{
+    Q_UNUSED(certFile);
+    return true;
+}
+
+const QString Device::cert_info() const
+{
+    return "";
+}
+
+bool Device::is_valid() const
+{
+    return true;
+}
+
+Device::Device()
+    : d(new DevicePrivate())
+{
+
+}
+Device::~Device()
+{
+    delete d;
+}
