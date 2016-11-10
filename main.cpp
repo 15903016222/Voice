f5410c657ddb23c17ae862623eaee0c0e229d74d
yanshaoyi <yanshaoyi@sw.cndoppler.cn>
2016-11-10 16:30:46 +0800

    Added:增加中文字体，设置编码格式和默认字体，切换中文时，在设备端将可以显示中文

--- main.cpp
+++ main.cpp
@@ -6,7 +6,14 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
+    int fontId = QFontDatabase::addApplicationFont(":/file/resources/SIMSUN.TTC");
+    QString msyh = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    QFont font(msyh, 16);
+
     MainWindow w;
+    w.setFont(font);
 #if (PHASCAN | PHASCAN_II)
     w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 #endif
