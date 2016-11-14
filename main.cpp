4313da0aabfa488fe78d2f393e244d8beb171e9e
Shaoyi Yan <yanshaoyi@sw.cndoppler.cn>
2016-11-14 14:52:01 +0800

    Updated: 更新Resolution菜单的style;

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
     QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
 
-    qDebug()<<"search_current_Dir = "<<qApp->applicationDirPath();
-
     int fontId = QFontDatabase::addApplicationFont(qApp->applicationDirPath() +"/SONGTI.TTC");
     QString fontName = QFontDatabase::applicationFontFamilies(fontId).at(0);
     QFont font(fontName, 16);
