3fb53836524974277f827bbec8e5f8c29d265304
Jake Yang <yanghuanjie@cndoppler.cn>
2016-11-15 10:23:10 +0800

    Updated: Spi测试例子

--- fpga/test_spi/main.cpp
+++ fpga/test_spi/main.cpp
@@ -1,42 +1,77 @@
-#include <QCoreApplication>
 #include <QDebug>
 
-#include <unistd.h>
+#include "../fpga_spi.h"
 
-#include "../spi.h"
+static void test_spi(int argc, char **argv);
+static void test_fpga_spi(int argc, char **argv);
 
 int main(int argc, char *argv[])
 {
-//    char msg[] = {0xaa, 0xbb, 0xcc, 0xdd, 'a', 'b', 'c', 'd', 1, 2, 3, 4};
-    quint32 msg = 0xaabbccdd;
+    test_fpga_spi(argc, argv);
+}
 
-    QCoreApplication a(argc, argv);
+void test_fpga_spi(int argc, char **argv)
+{
+    char msg[800];
+    ::memset(msg, 0xbb, sizeof(msg));
+
+    int num = 800;
 
     if (argc < 2) {
-        qDebug()<<"test_spi [device]";
-        return -1;
+        qWarning()<<"test_spi [device] [data num]";
+        exit(-1);
+    }
+
+    if (argv[2] != NULL) {
+        num = atoi(argv[2]);
+        qDebug()<<"num="<<num;
+    }
+    FpgaSpi *spi = FpgaSpi::get_spi();
+    qDebug("Mode[%d] Bits[%d] Speed[%dkHz] LSB_First[%d]",
+           spi->mode(), spi->bits_per_word(), spi->speed()/1000, spi->is_lsb_first());
+
+    spi->write((char *)&msg, num);
+}
+
+void test_spi(int argc, char **argv)
+{
+//    char msg[] = {0xaa, 0xbb, 0xcc, 0xdd, 'a', 'b', 'c', 'd', 1, 2, 3, 4};
+//    quint32 msg = 0xaabbccdd;
+    char msg[800];
+    ::memset(msg, 0xbb, sizeof(msg));
+
+    int num = 800;
+
+    if (argc < 2) {
+        qDebug()<<"test_spi [device] [data_num]";
+        exit(1);
+    }
+
+    if (argv[2] != NULL) {
+        num = atoi(argv[2]);
+        qDebug()<<"num = "<<num;
     }
 
     Spi *m_spi = new Spi();
 
     if ( ! m_spi->open(argv[1]) ) {
         qDebug()<<"open"<<argv[1]<<"failed";
-        return -1;
+        exit(1);
     }
 
     if ( ! m_spi->set_mode(Spi::MODE3) ) {
         qDebug()<<"set mode failed";
-        return -1;
+        exit(1);
     }
 
     if ( ! m_spi->set_bits_per_word(32)) {
         qDebug()<<"set bits per word failed";
-        return -1;
+        exit(1);
     }
 
     if ( ! m_spi->set_speed(500*1000)) {
         qDebug()<<"set max speed failed";
-        return -1;
+        exit(1);
     }
 
 //    if ( ! m_spi->set_lsb_first(false) ) {
@@ -47,7 +82,5 @@ int main(int argc, char *argv[])
     qDebug("Mode[%d] Bits[%d] Speed[%dkHz] LSB_First[%d]",
            m_spi->mode(), m_spi->bits_per_word(), m_spi->speed()/1000, m_spi->is_lsb_first());
 
-    m_spi->write((char *)&msg, sizeof(msg));
-
-    return 0;
+    m_spi->write((char *)&msg, num);
 }
--- fpga/test_spi/test_spi.pro
+++ fpga/test_spi/test_spi.pro
@@ -1,5 +1,9 @@
-QT += core
+QT += core quick
 QT -= gui
+
+DEVICE = PHASCAN_II
+
+DEFINES += $$DEVICE
 
 CONFIG += c++11
 
@@ -7,10 +11,16 @@ TARGET = test_spi
 CONFIG += console
 CONFIG -= app_bundle
 
+target.path = /home/root
+
+INSTALLS += target
+
 TEMPLATE = app
 
 SOURCES += main.cpp \
-    ../spi.cpp
+    ../spi.cpp \
+    ../fpga_spi.cpp
 
 HEADERS += \
-    ../spi.h
+    ../spi.h \
+    ../fpga_spi.h
