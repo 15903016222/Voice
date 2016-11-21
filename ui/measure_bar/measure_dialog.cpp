fabc5df302dc4823684f31e176c86966e6e34f57
Jake Yang <yanghuanjie@cndoppler.cn>
2016-11-21 15:06:01 +0800

    Modified: MeasureDialog使用QTableWidget实现

--- ui/measure_bar/measure_dialog.cpp
+++ ui/measure_bar/measure_dialog.cpp
@@ -1,10 +1,6 @@
 #include "measure_dialog.h"
 #include "ui_measure_dialog.h"
 
-#include <QKeyEvent>
-#include <QApplication>
-#include <QDebug>
-
 MeasureDialog::MeasureDialog(QWidget *parent, MeasureDialog::MeasureType type) :
     QDialog(parent),
     ui(new Ui::MeasureDialog)
@@ -13,11 +9,7 @@ MeasureDialog::MeasureDialog(QWidget *parent, MeasureDialog::MeasureType type) :
 
     setWindowFlags( (windowFlags() & ~Qt::WindowContextHelpButtonHint) | Qt::FramelessWindowHint);
 
-    ui->listWidget->setCurrentRow(type);
-
-    pMcu = Mcu::get_mcu();
-    connect(pMcu, SIGNAL(rotary_event(Mcu::RotaryType)), this, SLOT(do_rotary_event(Mcu::RotaryType)));
-    connect(pMcu, SIGNAL(key_event(Mcu::KeyType)), this, SLOT(key_sure(Mcu::KeyType)));
+    ui->tableWidget->setCurrentCell(type, 0);
 }
 
 MeasureDialog::~MeasureDialog()
@@ -27,52 +19,15 @@ MeasureDialog::~MeasureDialog()
 
 MeasureDialog::MeasureType MeasureDialog::get_type() const
 {
-    return (MeasureDialog::MeasureType)ui->listWidget->currentRow();
+    return (MeasureDialog::MeasureType)ui->tableWidget->currentRow();
 }
 
 QString MeasureDialog::get_type_string()
 {
-    QString measureType = ui->listWidget->currentItem()->text();
-
-    if(measureType.contains(" ") == true) {
-        int index = measureType.indexOf(" ");
-        QString text = measureType.left(index);
-
-        if(measureType.contains("%") || measureType.contains("Percentage")) {
-            text = text + "\n(%)";
-        } else if(measureType.contains("dB")) {
-            text = text + "\n(dB)";
-        } else {
-            text = text + "\n(mm)";
-        }
-        m_measureTypeMap.insert(text, measureType);
-    } else {
-        m_measureTypeMap.insert(measureType, measureType);
-    }
-
-    return m_measureTypeMap.key(measureType);
+    return ui->tableWidget->verticalHeaderItem(ui->tableWidget->currentRow())->text();
 }
 
 void MeasureDialog::set_type(MeasureDialog::MeasureType type)
 {
-    ui->listWidget->setCurrentRow(type);
-}
-
-void MeasureDialog::do_rotary_event(Mcu::RotaryType type)
-{
-    if (type == Mcu::ROTARY_UP) {
-        QKeyEvent *event = new QKeyEvent(QEvent::KeyPress, Qt::Key_Up, Qt::NoModifier);
-        QApplication::sendEvent(ui->listWidget, event);
-
-    } else {
-        QKeyEvent *event = new QKeyEvent(QEvent::KeyPress, Qt::Key_Down, Qt::NoModifier);
-        QApplication::sendEvent(ui->listWidget, event);
-    }
-}
-
-void MeasureDialog::key_sure(Mcu::KeyType key)
-{
-    if(key == Mcu::KEY_SURE) {
-        accept();
-    }
+    ui->tableWidget->setCurrentCell(type, 0);
 }
