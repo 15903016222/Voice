eb847b585ef5fa82d86f751bd5b82f420189fb9d
Shaoyi Yan <yanshaoyi@sw.cndoppler.cn>
2016-11-22 13:37:01 +0800

    Updated: 在updae_translator()中注释了set_top_menu_font(),解决了切换翻译异常终止的问题；

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -228,7 +228,7 @@ void MainWindow::update_translator(QString string)
     }
     ui->retranslateUi(this);
     ui->widgetTopLeft->retranslate_top_menu_ui();
-    ui->widgetTopLeft->set_top_menu_font();
+ //   ui->widgetTopLeft->set_top_menu_font();
     firstSecondMenu->retranslate_main_menu_ui(string);
     commonMenuWidget->retranslate_common_menu_ui();
 
