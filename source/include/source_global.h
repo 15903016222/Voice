ff815160e041e2d073391cf9eb5f77d95786b3c4
Jake Yang <yanghuanjie@cndoppler.cn>
2016-12-08 14:35:51 +0800

    Added: LibSource for Phascan

---
+++ source/include/source_global.h
@@ -0,0 +1,14 @@
+#ifndef SOURCE_GLOBAL_H
+#define SOURCE_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#define SOURCESHARED_EXPORT
+
+//#if defined(SOURCE_LIBRARY)
+//#  define SOURCESHARED_EXPORT Q_DECL_EXPORT
+//#else
+//#  define SOURCESHARED_EXPORT Q_DECL_IMPORT
+//#endif
+
+#endif // SOURCE_GLOBAL_H
