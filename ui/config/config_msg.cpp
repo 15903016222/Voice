a0a8fb53dfd7b5e91c8d8da78da41d2bb6a67b94
zhongjingyu <zhongjingyu@sw.cndoppler.cn>
2016-12-14 17:14:59 +0800

    Optimized:ConfigMsg

--- ui/config/config_msg.cpp
+++ ui/config/config_msg.cpp
@@ -2,13 +2,11 @@
 
 #include "msgpack.h"
 
-#include <QFile>
 #include <QCoreApplication>
 #include <QDebug>
 
 ConfigMsg::ConfigMsg()
 {
-//    settings("ConfigMessage", QSettings::NativeFormat);
     QCoreApplication::setOrganizationName(QString("Mercury"));
     QSettings settings(QSettings::NativeFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
     qDebug() << "fileName" << settings.fileName();
@@ -21,30 +19,10 @@ ConfigMsg::~ConfigMsg()
 
 void ConfigMsg::add_config_msg(QString &groupName, QString &mainMenu, QString &subMenu, QVariant &variant)
 {
-//    QVariantMap t_map1, t_map2;
-//    if(m_mapConfig.contains(groupName)) {
-//        t_map1 = m_mapConfig[groupName].toMap();
-//        if(t_map1.contains(mainMenu)) {
-//            t_map2 = t_map1[mainMenu].toMap();
-//            if(t_map2.contains(subMenu)) {
-//                t_map2.remove(subMenu);
-//            }
-//        }
-//        t_map2.insert(subMenu, variant);
-//        t_map1.insert(mainMenu, t_map2);
-//    } else {
-//        t_map2.insert(subMenu, variant);
-//        t_map1.insert(mainMenu, t_map2);
-//    }
-//    m_mapConfig.insert(groupName, t_map1);
-
-//    m_array = MsgPack::pack(m_mapConfig);
-
     QVariantMap map = get_map(groupName, mainMenu);
     map.insert(subMenu, variant);
 
     QSettings settings(QSettings::NativeFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
-    qDebug() << settings.fileName();
     settings.beginGroup(groupName);
     settings.setValue(mainMenu, MsgPack::pack(map));
     settings.endGroup();
@@ -52,36 +30,18 @@ void ConfigMsg::add_config_msg(QString &groupName, QString &mainMenu, QString &s
     map.clear();
 }
 
-//void ConfigMsg::check_map(QString &groupName, QString &mainMenu, QString &subMenu)
-//{
-//    QVariantMap t_map1, t_map2;
-//    if(m_mapConfig.contains(groupName)) {
-//        t_map1 = m_mapConfig[groupName].toMap();
-//        if(t_map1.contains(mainMenu)) {
-//            t_map2 = t_map1[mainMenu].toMap();
-//            if(t_map2.contains(subMenu)) {
-//                t_map2.remove(subMenu);
-//            }
-//        }
-//        t_map1.insert(mainMenu, t_map2);
-//        m_mapConfig.insert(groupName, t_map1);
-//    }
-//}
+void ConfigMsg::add_config_msg(QString &groupName, QString &mainMenu, QVariantMap map)
+{
+    QSettings settings(QSettings::NativeFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
+    settings.beginGroup(groupName);
+    settings.setValue(mainMenu, MsgPack::pack(map));
+    settings.endGroup();
+    map.clear();
+}
 
 QVariant ConfigMsg::get_config_msg(QString &groupName, QString &mainMenu, QString &subMenu)
 {
     QVariant variant;
-//    QVariantMap map = MsgPack::unpack(m_array).toMap();
-//    if(map.contains(groupName)) {
-//        QVariantMap t_map1 = map[groupName].toMap();
-//        if(t_map1.contains(mainMenu)) {
-//            QVariantMap t_map2 = t_map1[mainMenu].toMap();
-//            if(t_map2.contains(subMenu)) {
-//                variant = t_map2.value(subMenu);
-//            }
-//        }
-//    }
-
     QVariantMap map = get_map(groupName, mainMenu);
     if(map.contains(subMenu)) {
         variant = map.value(subMenu);
@@ -94,33 +54,16 @@ QVariant ConfigMsg::get_config_msg(QString &groupName, QString &mainMenu, QStrin
 QVariantMap ConfigMsg::get_map(QString &groupName, QString &mainMenu)
 {
     QVariantMap map;
-//    QVariantMap t_map = MsgPack::unpack(m_array).toMap();
-//    if(t_map.contains(groupName)) {
-//        QVariantMap t_map1 = t_map[groupName].toMap();
-//        if(t_map1.contains(mainMenu)) {
-//            map = t_map1[mainMenu].toMap();
-//        }
-//    }
     QSettings settings(QSettings::NativeFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
     QVariant variant = settings.value(groupName + QString("/") + mainMenu);
     if(!variant.isNull()) {
         map = MsgPack::unpack(variant.toByteArray()).toMap();
     }
+    variant.clear();
     return map;
 }
 
-//QVariant ConfigMsg::get_message(QVariantMap map, QString &subMenu)
-//{
-//    QVariant variant;
-//    if(!map.isEmpty() && map.contains(subMenu)) {
-//        variant = map.value(subMenu);
-//    }
-//    qDebug() << variant;
-//    return variant;
-//}
-
-//void ConfigMsg::clear_config_msg()
-//{
-//    m_array.clear();
-//    m_mapConfig.clear();
-//}
+void ConfigMsg::insert_msg_to_map(QVariantMap map, QString &subMenu, QVariant variant)
+{
+    map.insert(subMenu, variant);
+}
--- ui/config/config_msg.h
+++ ui/config/config_msg.h
@@ -14,16 +14,13 @@ public:
     ~ConfigMsg();
 
     void add_config_msg(QString &groupName, QString &mainMenu, QString &subMenu, QVariant &variant);
-//    void check_map(QString &groupName, QString &mainMenu, QString &subMenu);
+    void add_config_msg(QString &groupName, QString &mainMenu, QVariantMap &map);
+    void insert_msg_to_map(QVariantMap &map, QString &subMenu, QVariant &variant);
+
     QVariant get_config_msg(QString &groupName, QString &mainMenu, QString &subMenu);
     QVariantMap get_map(QString &groupName, QString &mainMenu);
-//    QVariant get_message(QVariantMap map, QString &subMenu);
-//    void clear_config_msg();
 
 private:
-//    QVariantMap m_mapConfig;
-//    QByteArray m_array;
-//    QSettings settings;
 
 };
 
