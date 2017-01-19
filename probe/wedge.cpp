4bc20bc65f6ca36a3b2b1c3b6eb6099dcd2c2a71
Jake Yang <yanghuanjie@cndoppler.cn>
2017-01-19 16:26:50 +0800

    修改: Probe构造函数初始化成员变量

--- probe/wedge.cpp
+++ probe/wedge.cpp
@@ -2,15 +2,55 @@
 
 namespace DplProbe {
 
+struct WedgeData
+{
+    char    res0;
+    qint8   type;           /* 1 是L 2 是 S */
+    char    res1;
+    char    model[20];      /* 共用 楔块名字 */
+    char    serial[20];     /* 共用 楔块名字 */
+    quint16 angle;          /* 共用 角度单位0.1度 */
+    quint16 res2;
+    quint16 probeDelay;     /* UT ns为单位 */
+    char    res3;
+    char	res4;           /* UT 1 SW 0 LW*/
+    int	Ref_point;          /* UT 使用 */
+    char    res5;
+    qint32	Height;			/* 单位微米 */
+    unsigned int	Velocity_UT;
+    unsigned int	A8;
+    unsigned int	Velocity_PA;	/* 速度 mm/s */
+    char	Orientation;	/* 1 Normal 0 reversal*/
+    char	A4[3];
+    int	Primary_offset;	/* 微米 */
+    unsigned int	Secondary_offset;	/* 微米 */
+    unsigned int    length;
+    int	A6[106];
+};
+
 Wedge::Wedge(QObject *parent) :
     QObject(parent)
 {
-    m_delay = 0;
+    m_delay     = 0;
+
+    m_waveType = Longitudinal;
+    m_orientation = Normal;
+    m_velocity  = 0;
+    m_primaryOffset = 0.0;
+    m_secondaryOffset = 0.0;
+    m_height    = 0.0;
+    m_length    = 0.0;
+    m_refPoint  = 0.0;
 }
 
 Wedge::~Wedge()
 {
 
+}
+
+bool Wedge::load(const QString &fileName)
+{
+    return true;
 }
 
 }
--- probe/wedge.h
+++ probe/wedge.h
@@ -19,6 +19,59 @@ public:
     ~Wedge();
 
     /**
+     * @brief serial    获取系列号
+     * @return          系列号
+     */
+    const QString &serial() const;
+
+    /**
+     * @brief set_serial    设置系列号
+     * @param val           系列号
+     */
+    void set_serial(const QString &val);
+
+    /**
+     * @brief model 获取型号
+     * @return      型号
+     */
+    const QString &model() const;
+
+    /**
+     * @brief set_model 设置型号
+     * @param val       型号
+     */
+    void set_model(const QString &val);
+
+    enum WaveType {
+        Longitudinal,
+        Transverse
+    };
+
+    /**
+     * @brief wave_type 获取波类型
+     * @return          类型
+     */
+    WaveType wave_type() const;
+
+    /**
+     * @brief set_wave_type
+     * @param type
+     */
+    void set_wave_type(WaveType type);
+
+    /**
+     * @brief angel 获取角度
+     * @return      角度值，单位(度)
+     */
+    float angel() const;
+
+    /**
+     * @brief set_angel 设置角度
+     * @param val       角度值，单位(度)
+     */
+    void set_angel(float val);
+
+    /**
      * @brief delay 楔块延迟时间
      * @return      返回楔块延迟时间，单位(ns)
      */
@@ -30,13 +83,165 @@ public:
      */
     void set_delay(int value);
 
+    enum Orientation {
+        Normal,
+        Reversal
+    };
+    /**
+     * @brief orientation   获取探头方向
+     * @return              方向
+     */
+    Orientation orientation() const;
+
+    /**
+     * @brief set_orientation   设置探头方向
+     * @param val               方向
+     */
+    void set_orientation(Orientation val);
+
+    /**
+     * @brief velocity  获取声速
+     * @return          声速值，单位(m/s)
+     */
+    int velocity() const;
+
+    /**
+     * @brief set_velocity  设置声速
+     * @param val           声速值,单位(m/s)
+     */
+    void set_velocity(int val);
+
+    /**
+     * @brief primary_offset    主轴偏置
+     * @return                  偏置值，单位(mm)
+     */
+    float primary_offset()const;
+
+    /**
+     * @brief set_primary_offset    设置主轴偏置
+     * @param val                   偏置值，单位(mm)
+     */
+    void set_primary_offset(float val);
+
+    /**
+     * @brief secondary_offset  次轴偏置
+     * @return                  偏置，单位(mm)
+     */
+    float secondary_offset() const;
+
+    /**
+     * @brief set_secondary_offset  设置次轴偏置
+     * @param val                   偏置，单位(mm)
+     */
+    void set_secondary_offset(float val);
+
+    /**
+     * @brief height    获取第一阵元高度
+     * @return          高度，单位(mm)
+     */
+    float height() const;
+
+    /**
+     * @brief set_height    设置第一阵元高度
+     * @param val           高度值，单位(mm)
+     */
+    void set_height(float val);
+
+    /**
+     * @brief length    获取长度
+     * @return          长度，单位(mm)
+     */
+    float length() const;
+
+    /**
+     * @brief set_lenght    设置长度
+     * @param val           长度值，单位(mm)
+     */
+    void set_lenght(float val);
+
+    /**
+     * @brief ref_point 获取参考点
+     * @return          参考点值，单位(mm)
+     */
+    float ref_point() const;
+
+    /**
+     * @brief set_ref_point 设置参考点
+     * @param val           参考点值，单位(mm)
+     */
+    void set_ref_point(float val);
+
+    /**
+     * @brief load      加载楔块文件
+     * @param fileName  文件名
+     * @return          成功返回true,失败返回false
+     */
+    bool load(const QString &fileName);
+
+    /**
+     * @brief save      保存楔块文件
+     * @param fileName  文件名
+     * @return          成功返回true，失败返回false
+     */
+    bool save(const QString &fileName);
+
 signals:
     void delay_changed(int value);
 
 private:
-    int m_delay;        /* 延迟时间，单位(ns) */
-
+    QString m_serial;           /* 系列 */
+    QString m_model;            /* 型号 */
+    WaveType m_waveType;        /* 波类型 */
+    float m_angle;              /* 角度，单位(度) */
+    int m_delay;                /* 延迟时间，单位(ns) */
+    Orientation m_orientation;  /* 方向 */
+    int m_velocity;             /* 声速,(m/s) */
+    float m_primaryOffset;      /* 主轴偏置,(mm) */
+    float m_secondaryOffset;    /* 次轴偏置,(mm) */
+    float m_height;             /* 高度，(mm) */
+    float m_length;             /* 长度，(mm) */
+    float m_refPoint;           /* 参考点，(mm) */
 };
+
+inline const QString &Wedge::serial() const
+{
+    return m_serial;
+}
+
+inline void Wedge::set_serial(const QString &val)
+{
+    m_serial = val;
+}
+
+inline const QString &Wedge::model() const
+{
+    return m_model;
+}
+
+inline void Wedge::set_model(const QString &val)
+{
+    m_model = val;
+}
+
+inline Wedge::WaveType Wedge::wave_type() const
+{
+    return m_waveType;
+}
+
+inline void Wedge::set_wave_type(Wedge::WaveType type)
+{
+    m_waveType = type;
+}
+
+inline float Wedge::angel() const
+{
+    return m_angle;
+}
+
+inline void Wedge::set_angel(float val)
+{
+    m_angle = val;
+}
 
 inline int Wedge::delay() const
 {
@@ -50,6 +255,78 @@ inline void Wedge::set_delay(int value)
         emit delay_changed(value);
     }
 }
+
+inline Wedge::Orientation Wedge::orientation() const
+{
+    return m_orientation;
+}
+
+inline void Wedge::set_orientation(Wedge::Orientation val)
+{
+    m_orientation = val;
+}
+
+inline int Wedge::velocity() const
+{
+    return m_velocity;
+}
+
+inline void Wedge::set_velocity(int val)
+{
+    m_velocity = val;
+}
+
+inline float Wedge::primary_offset() const
+{
+    return m_primaryOffset;
+}
+
+inline void Wedge::set_primary_offset(float val)
+{
+    m_primaryOffset = val;
+}
+
+inline float Wedge::secondary_offset() const
+{
+    return m_secondaryOffset;
+}
+
+inline void Wedge::set_secondary_offset(float val)
+{
+    m_secondaryOffset = val;
+}
+
+inline float Wedge::height() const
+{
+    return m_height;
+}
+
+inline void Wedge::set_height(float val)
+{
+    m_height = val;
+}
+
+inline float Wedge::length() const
+{
+    return m_length;
+}
+
+inline void Wedge::set_lenght(float val)
+{
+    m_length = val;
+}
+
+inline float Wedge::ref_point() const
+{
+    return m_refPoint;
+}
+
+inline void Wedge::set_ref_point(float val)
+{
+    m_refPoint = val;
+}
+
+
 
 }
 
