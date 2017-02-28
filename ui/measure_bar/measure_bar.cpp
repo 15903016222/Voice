597fad2e7b288b51009fe85a458099fbb5542076
zhongjingyu <zhongjingyu@sw.cndoppler.cn>
2017-02-28 09:59:24 +0800

    Added:在MeasureBar增加do_beamgroup_data_event槽函数

--- ui/measure_bar/measure_bar.cpp
+++ ui/measure_bar/measure_bar.cpp
@@ -11,20 +11,16 @@ MeasureBar :: MeasureBar(QWidget *parent) :
 {
     ui->setupUi(this);
 
-//    measureCalculation = MeasureCalculation::get_calculation();
-//    qDebug() << "1";
-
     init_map();
 
     DplDevice::Device *device = DplDevice::Device::get_instance();
     connect(device, SIGNAL(current_group_changed()), this, SLOT(do_current_group_changed()));
-//    m_group = device->current_group();
+    m_group = device->current_group();
+//    m_beamNo = 0;
+    m_beamIndex = 0;
 
-//    DplSource::Source *source = DplSource::Source::get_instance();
-//    connect(source, SIGNAL(type_changed(DplSource::Source::Type)), this, SLOT(do_source_type_changed(DplSource::Source::Type)));
-
-//    DplSource::BeamGroupPointer beamGroup = device->current_group()->get_beam_group();
-//    connect(beamGroup, SIGNAL())
+    m_beamGroup = m_group->get_beam_group();
+    connect(static_cast<DplSource::BeamGroup *>(m_beamGroup.data()), SIGNAL(data_event()), this, SLOT(do_beamgroup_data_event()));
 
     connect(ui->measureWidget1, SIGNAL(type_changed(MeasureDialog::MeasureType)), this, SLOT(do_type_changed(MeasureDialog::MeasureType)));
     connect(ui->measureWidget2, SIGNAL(type_changed(MeasureDialog::MeasureType)), this, SLOT(do_type_changed(MeasureDialog::MeasureType)));
@@ -36,13 +32,16 @@ MeasureBar :: MeasureBar(QWidget *parent) :
     connect(ui->measureWidget8, SIGNAL(type_changed(MeasureDialog::MeasureType)), this, SLOT(do_type_changed(MeasureDialog::MeasureType)));
 
     ui->measureWidget1->set_type(MeasureDialog::Gate_A_Peak);
-    ui->measureWidget2->set_type(MeasureDialog::DA);
-    ui->measureWidget3->set_type(MeasureDialog::PA);
-    ui->measureWidget4->set_type(MeasureDialog::SA);
+    ui->measureWidget2->set_type(MeasureDialog::Gate_AdBA);
+    ui->measureWidget3->set_type(MeasureDialog::Gate_B_Peak);
+    ui->measureWidget4->set_type(MeasureDialog::Gate_BdBB);
+//    ui->measureWidget2->set_type(MeasureDialog::DA);
+//    ui->measureWidget3->set_type(MeasureDialog::PA);
+//    ui->measureWidget4->set_type(MeasureDialog::SA);
     ui->measureWidget5->set_type(MeasureDialog::Measurement_Cursor_UT_Position);
     ui->measureWidget6->set_type(MeasureDialog::Measurement_Cursor_Index_Position);
     ui->measureWidget7->set_type(MeasureDialog::Measurement_Cursor_Scan_Position);
-    ui->measureWidget8->set_type(MeasureDialog::Gate_B_Peak);
+    ui->measureWidget8->set_type(MeasureDialog::Gate_A_Position);
 }
 
 MeasureBar::~MeasureBar()
@@ -53,83 +52,106 @@ MeasureBar::~MeasureBar()
 void MeasureBar::do_type_changed(MeasureDialog::MeasureType type)
 {
     MeasureWidget *widget = qobject_cast<MeasureWidget*>(sender());
-    widget->set_value(QString::number(calculate_value(type), 'f', 1));
+    if(m_beamGroup->get(0).isNull()) {
+        return;
+    } else {
+        widget->set_value(calculate_value(type));
+    }
 }
 
-double MeasureBar::calculate_value(MeasureDialog::MeasureType type)
+QString MeasureBar::calculate_value(MeasureDialog::MeasureType type)
 {
     MeasureCalculation::Function pFun = m_map.value(type);
     if(pFun) {
-        return (MeasureCalculation::get_calculation()->*pFun)();
+        double value = (MeasureCalculation::get_calculation()->*pFun)(m_group, m_beamIndex);
+        return QString::number(value, 'f', 1);
     }
 }
 
 void MeasureBar::do_current_group_changed()
 {
-    ui->measureWidget1->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget2->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget3->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget4->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget5->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget6->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget7->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
-    ui->measureWidget8->set_value(QString::number(calculate_value(ui->measureWidget1->type()), 'f', 1));
+    m_group = DplDevice::Device::get_instance()->current_group();
+    m_beamGroup = m_group->get_beam_group();
+}
+
+//void MeasureBar::do_beam_qty_changed(int qty)
+//{
+////    m_beamNo = qty;
+//    m_beamIndex = qty;
+//    set();
+//}
+
+void MeasureBar::do_beamgroup_data_event()
+{
+    set();
+}
+
+void MeasureBar::set()
+{
+    ui->measureWidget1->set_value(calculate_value(ui->measureWidget1->type()));
+    ui->measureWidget2->set_value(calculate_value(ui->measureWidget2->type()));
+    ui->measureWidget3->set_value(calculate_value(ui->measureWidget3->type()));
+    ui->measureWidget4->set_value(calculate_value(ui->measureWidget4->type()));
+    ui->measureWidget5->set_value(calculate_value(ui->measureWidget5->type()));
+    ui->measureWidget6->set_value(calculate_value(ui->measureWidget6->type()));
+    ui->measureWidget7->set_value(calculate_value(ui->measureWidget7->type()));
+    ui->measureWidget8->set_value(calculate_value(ui->measureWidget8->type()));
 }
 
 void MeasureBar::init_map()
 {
-    m_map.insert(MeasureDialog::Gate_A_Peak, &MeasureCalculation::set_gate_a_peak_value);
-    m_map.insert(MeasureDialog::Gate_AdBA, &MeasureCalculation::set_gate_adBa_value);
-    m_map.insert(MeasureDialog::Gate_AdBr, &MeasureCalculation::set_gate_adBr_value);
-    m_map.insert(MeasureDialog::Gate_B_Peak, &MeasureCalculation::set_gate_b_peak_value);
-    m_map.insert(MeasureDialog::Gate_BdBB, &MeasureCalculation::set_gate_bdBb_value);
-    m_map.insert(MeasureDialog::Gate_BdBr, &MeasureCalculation::set_gate_bdBr_value);
-    m_map.insert(MeasureDialog::Gate_A_Position, &MeasureCalculation::set_gate_a_position_value);
-    m_map.insert(MeasureDialog::Gate_B_Position, &MeasureCalculation::set_gate_b_position_value);
-    m_map.insert(MeasureDialog::Gate_I_Position, &MeasureCalculation::set_gate_i_position_value);
-    m_map.insert(MeasureDialog::Gate_I_Water_Position, &MeasureCalculation::set_gate_i_water_position_value);
-    m_map.insert(MeasureDialog::Thickness, &MeasureCalculation::set_thickness_value);
-    m_map.insert(MeasureDialog::ML, &MeasureCalculation::set_ml_value);
-    m_map.insert(MeasureDialog::Reference_Cursor_Amplitude, &MeasureCalculation::set_reference_cursor_amplitude_value);
-    m_map.insert(MeasureDialog::Measurement_Cursor_Amplitude, &MeasureCalculation::set_measurement_cursor_amplitude_value);
-    m_map.insert(MeasureDialog::Measurement_Reference, &MeasureCalculation::set_measurement_reference_value);
-    m_map.insert(MeasureDialog::Reference_Cursor_UT_Position, &MeasureCalculation::set_reference_cursor_ut_position_value);
-    m_map.insert(MeasureDialog::Measurement_Cursor_UT_Position, &MeasureCalculation::set_measurement_cursor_ut_position_value);
-    m_map.insert(MeasureDialog::Measurement_Reference_UT_Position, &MeasureCalculation::set_measurement_reference_ut_position_value);
-    m_map.insert(MeasureDialog::Reference_Cursor_Probe_Position, &MeasureCalculation::set_reference_cursor_probe_position_value);
-    m_map.insert(MeasureDialog::Measurement_Cursor_Probe_Position, &MeasureCalculation::set_measurement_cursor_probe_position_value);
-    m_map.insert(MeasureDialog::Measurement_Reference_Probe_position, &MeasureCalculation::set_measurement_reference_probe_position_value);
-    m_map.insert(MeasureDialog::Reference_Cursor_Scan_Position, &MeasureCalculation::set_reference_cursor_scan_position_value);
-    m_map.insert(MeasureDialog::Measurement_Cursor_Scan_Position, &MeasureCalculation::set_measurement_cursor_scan_position_value);
-    m_map.insert(MeasureDialog::Measurement_Reference_Scan_Position, &MeasureCalculation::set_measurement_reference_scan_position_value);
-    m_map.insert(MeasureDialog::Reference_Cursor_Index_Position, &MeasureCalculation::set_reference_cursor_index_position_value);
-    m_map.insert(MeasureDialog::Measurement_Cursor_Index_Position, &MeasureCalculation::set_measurement_cursor_index_position_value);
-    m_map.insert(MeasureDialog::Measurement_Reference_Index_Position, &MeasureCalculation::set_measurement_reference_index_position_value);
-    m_map.insert(MeasureDialog::RA, &MeasureCalculation::set_ra_value);
-    m_map.insert(MeasureDialog::RB, &MeasureCalculation::set_rb_value);
-    m_map.insert(MeasureDialog::PA, &MeasureCalculation::set_pa_value);
-    m_map.insert(MeasureDialog::PB, &MeasureCalculation::set_pb_value);
-    m_map.insert(MeasureDialog::DA, &MeasureCalculation::set_da_value);
-    m_map.insert(MeasureDialog::DB, &MeasureCalculation::set_db_value);
-    m_map.insert(MeasureDialog::SA, &MeasureCalculation::set_sa_value);
-    m_map.insert(MeasureDialog::SB, &MeasureCalculation::set_sb_value);
-    m_map.insert(MeasureDialog::ViA, &MeasureCalculation::set_via_value);
-    m_map.insert(MeasureDialog::ViB, &MeasureCalculation::set_vib_value);
-    m_map.insert(MeasureDialog::VsA, &MeasureCalculation::set_vsa_value);
-    m_map.insert(MeasureDialog::VsB, &MeasureCalculation::set_vsb_value);
-    m_map.insert(MeasureDialog::LA, &MeasureCalculation::set_la_value);
-    m_map.insert(MeasureDialog::LB, &MeasureCalculation::set_lb_value);
-    m_map.insert(MeasureDialog::E, &MeasureCalculation::set_e_value);
-    m_map.insert(MeasureDialog::AWS_DA, &MeasureCalculation::set_aws_da_value);
-    m_map.insert(MeasureDialog::AWS_DB, &MeasureCalculation::set_aws_db_value);
-    m_map.insert(MeasureDialog::AWS_DC, &MeasureCalculation::set_aws_dc_value);
-    m_map.insert(MeasureDialog::AWS_DD, &MeasureCalculation::set_aws_dd_value);
-    m_map.insert(MeasureDialog::AWS_D45, &MeasureCalculation::set_aws_d45_value);
-    m_map.insert(MeasureDialog::AWS_D60, &MeasureCalculation::set_aws_d60_value);
-    m_map.insert(MeasureDialog::AWS_D70, &MeasureCalculation::set_aws_d70_value);
-    m_map.insert(MeasureDialog::AWS_DCL, &MeasureCalculation::set_aws_dcl_value);
-    m_map.insert(MeasureDialog::T_D_r, &MeasureCalculation::set_t_d_r_value);
-    m_map.insert(MeasureDialog::T_D_m, &MeasureCalculation::set_t_d_m_value);
-    m_map.insert(MeasureDialog::T_S_mr, &MeasureCalculation::set_t_s_mr_value);
-    m_map.insert(MeasureDialog::T_D_mr, &MeasureCalculation::set_t_d_mr_value);
+    m_map.insert(MeasureDialog::Gate_A_Peak, &MeasureCalculation::get_gate_a_peak_value);
+    m_map.insert(MeasureDialog::Gate_AdBA, &MeasureCalculation::get_gate_adBa_value);
+    m_map.insert(MeasureDialog::Gate_AdBr, &MeasureCalculation::get_gate_adBr_value);
+    m_map.insert(MeasureDialog::Gate_B_Peak, &MeasureCalculation::get_gate_b_peak_value);
+    m_map.insert(MeasureDialog::Gate_BdBB, &MeasureCalculation::get_gate_bdBb_value);
+    m_map.insert(MeasureDialog::Gate_BdBr, &MeasureCalculation::get_gate_bdBr_value);
+    m_map.insert(MeasureDialog::Gate_A_Position, &MeasureCalculation::get_gate_a_position_value);
+    m_map.insert(MeasureDialog::Gate_B_Position, &MeasureCalculation::get_gate_b_position_value);
+    m_map.insert(MeasureDialog::Gate_I_Position, &MeasureCalculation::get_gate_i_position_value);
+    m_map.insert(MeasureDialog::Gate_I_Water_Position, &MeasureCalculation::get_gate_i_water_position_value);
+    m_map.insert(MeasureDialog::Thickness, &MeasureCalculation::get_thickness_value);
+    m_map.insert(MeasureDialog::ML, &MeasureCalculation::get_ml_value);
+    m_map.insert(MeasureDialog::Reference_Cursor_Amplitude, &MeasureCalculation::get_reference_cursor_amplitude_value);
+    m_map.insert(MeasureDialog::Measurement_Cursor_Amplitude, &MeasureCalculation::get_measurement_cursor_amplitude_value);
+    m_map.insert(MeasureDialog::Measurement_Reference, &MeasureCalculation::get_measurement_reference_value);
+    m_map.insert(MeasureDialog::Reference_Cursor_UT_Position, &MeasureCalculation::get_reference_cursor_ut_position_value);
+    m_map.insert(MeasureDialog::Measurement_Cursor_UT_Position, &MeasureCalculation::get_measurement_cursor_ut_position_value);
+    m_map.insert(MeasureDialog::Measurement_Reference_UT_Position, &MeasureCalculation::get_measurement_reference_ut_position_value);
+    m_map.insert(MeasureDialog::Reference_Cursor_Probe_Position, &MeasureCalculation::get_reference_cursor_probe_position_value);
+    m_map.insert(MeasureDialog::Measurement_Cursor_Probe_Position, &MeasureCalculation::get_measurement_cursor_probe_position_value);
+    m_map.insert(MeasureDialog::Measurement_Reference_Probe_position, &MeasureCalculation::get_measurement_reference_probe_position_value);
+    m_map.insert(MeasureDialog::Reference_Cursor_Scan_Position, &MeasureCalculation::get_reference_cursor_scan_position_value);
+    m_map.insert(MeasureDialog::Measurement_Cursor_Scan_Position, &MeasureCalculation::get_measurement_cursor_scan_position_value);
+    m_map.insert(MeasureDialog::Measurement_Reference_Scan_Position, &MeasureCalculation::get_measurement_reference_scan_position_value);
+    m_map.insert(MeasureDialog::Reference_Cursor_Index_Position, &MeasureCalculation::get_reference_cursor_index_position_value);
+    m_map.insert(MeasureDialog::Measurement_Cursor_Index_Position, &MeasureCalculation::get_measurement_cursor_index_position_value);
+    m_map.insert(MeasureDialog::Measurement_Reference_Index_Position, &MeasureCalculation::get_measurement_reference_index_position_value);
+    m_map.insert(MeasureDialog::RA, &MeasureCalculation::get_ra_value);
+    m_map.insert(MeasureDialog::RB, &MeasureCalculation::get_rb_value);
+    m_map.insert(MeasureDialog::PA, &MeasureCalculation::get_pa_value);
+    m_map.insert(MeasureDialog::PB, &MeasureCalculation::get_pb_value);
+    m_map.insert(MeasureDialog::DA, &MeasureCalculation::get_da_value);
+    m_map.insert(MeasureDialog::DB, &MeasureCalculation::get_db_value);
+    m_map.insert(MeasureDialog::SA, &MeasureCalculation::get_sa_value);
+    m_map.insert(MeasureDialog::SB, &MeasureCalculation::get_sb_value);
+    m_map.insert(MeasureDialog::ViA, &MeasureCalculation::get_via_value);
+    m_map.insert(MeasureDialog::ViB, &MeasureCalculation::get_vib_value);
+    m_map.insert(MeasureDialog::VsA, &MeasureCalculation::get_vsa_value);
+    m_map.insert(MeasureDialog::VsB, &MeasureCalculation::get_vsb_value);
+    m_map.insert(MeasureDialog::LA, &MeasureCalculation::get_la_value);
+    m_map.insert(MeasureDialog::LB, &MeasureCalculation::get_lb_value);
+    m_map.insert(MeasureDialog::E, &MeasureCalculation::get_e_value);
+    m_map.insert(MeasureDialog::AWS_DA, &MeasureCalculation::get_aws_da_value);
+    m_map.insert(MeasureDialog::AWS_DB, &MeasureCalculation::get_aws_db_value);
+    m_map.insert(MeasureDialog::AWS_DC, &MeasureCalculation::get_aws_dc_value);
+    m_map.insert(MeasureDialog::AWS_DD, &MeasureCalculation::get_aws_dd_value);
+    m_map.insert(MeasureDialog::AWS_D45, &MeasureCalculation::get_aws_d45_value);
+    m_map.insert(MeasureDialog::AWS_D60, &MeasureCalculation::get_aws_d60_value);
+    m_map.insert(MeasureDialog::AWS_D70, &MeasureCalculation::get_aws_d70_value);
+    m_map.insert(MeasureDialog::AWS_DCL, &MeasureCalculation::get_aws_dcl_value);
+    m_map.insert(MeasureDialog::T_D_r, &MeasureCalculation::get_t_d_r_value);
+    m_map.insert(MeasureDialog::T_D_m, &MeasureCalculation::get_t_d_m_value);
+    m_map.insert(MeasureDialog::T_S_mr, &MeasureCalculation::get_t_s_mr_value);
+    m_map.insert(MeasureDialog::T_D_mr, &MeasureCalculation::get_t_d_mr_value);
 }
--- ui/measure_bar/measure_bar.h
+++ ui/measure_bar/measure_bar.h
@@ -24,15 +24,21 @@ private:
     Ui::MeasureBar *ui;
 //    MeasureCalculation *measureCalculation;
     QMap<MeasureDialog::MeasureType, MeasureCalculation::Function> m_map;
-//    DplDevice::GroupPointer *m_group;
+    DplDevice::GroupPointer m_group;
+    DplSource::BeamGroupPointer m_beamGroup;
+    int m_beamIndex;
+    int m_beamNo;
 
     void init_map();
-    double calculate_value(MeasureDialog::MeasureType type);
+    void set();
+
+    QString calculate_value(MeasureDialog::MeasureType type);
 
 public slots:
     void do_type_changed(MeasureDialog::MeasureType type);
     void do_current_group_changed();
-//    void do_source_type_changed(DplSource::Source::Type);
+//    void do_beam_qty_changed(int qty);
+    void do_beamgroup_data_event();
 
 };
 
