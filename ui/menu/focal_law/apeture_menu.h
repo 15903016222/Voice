a14c167dcd4a6e4dbc7e352605be4ba1a855fe16
Jake Yang <yanghuanjie@cndoppler.cn>
2017-08-21 13:22:15 +0800

    更新: ApertureMenu

--- ui/menu/focal_law/apeture_menu.h
+++ ui/menu/focal_law/apeture_menu.h
@@ -2,6 +2,7 @@
 #define __APETURE_MENU_H__
 
 #include "../base_menu.h"
+#include <device/device.h>
 
 namespace DplFocalLawMenu {
 
@@ -12,14 +13,21 @@ public:
     explicit ApetureMenu(QWidget *parent);
     ~ApetureMenu();
 
-    void show();
-    void hide();
+protected:
+    void update_apetureItem(const DplFocallaw::ScanCnfPointer &scanCnf);
+    void update_firstElementItem(const DplFocallaw::ScanCnfPointer &scanCnf);
+    void update_lastElementItem(const DplFocallaw::LinearScanCnfPointer &scanCnf);
+    void update_elementStep(const DplFocallaw::LinearScanCnfPointer &scanCnf);
+
+protected slots:
+    void update(const DplDevice::GroupPointer &group);
 
 private:
     SpinMenuItem *m_apetureItem;
     SpinMenuItem *m_firstElementItem;
     SpinMenuItem *m_lastElementItem;
     SpinMenuItem *m_elementStep;
+    DplDevice::GroupPointer m_group;
 };
 
 }
