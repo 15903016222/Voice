283de201491320065ebad1e23fa06ed93eeb291f
Jake Yang <yanghuanjie@cndoppler.cn>
2017-10-19 19:29:04 +0800

    修改: 将数据处理从MeasureBar移到MeasureWidget

--- ui/measure_bar/measure_bar.h
+++ ui/measure_bar/measure_bar.h
@@ -29,19 +29,8 @@ public:
 public slots:
     void do_measureWidget_clicked(MeasureWidget *w);
 
-    void do_current_group_changed(const DplDevice::GroupPointer &group);
-    void do_beamgroup_data_event();
-
-signals:
-    void calculated(MeasureWidget *w, const QString &str);
-
-protected slots:
-    void do_calculated(MeasureWidget *w, const QString &str);
-
 private:
     Ui::MeasureBar *ui;
-
-    DplDevice::GroupPointer m_group;
 
     void set_measure_widget(MeasureWidget *w, Measure::Type type);
     void set_measure_widget(MeasureWidget *w, MeasureDialog &dlg);
