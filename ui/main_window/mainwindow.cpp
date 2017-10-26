54f789a23fb2af0c351d3e903285640812b0d11f
Jake Yang <yanghuanjie@cndoppler.cn>
2017-10-26 09:28:07 +0800

    增加右键鼠标切换显示菜单

--- ui/main_window/mainwindow.cpp
+++ ui/main_window/mainwindow.cpp
@@ -82,6 +82,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    if (e->button() == Qt::RightButton) {
+        show_hidden_Menu();
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Alt) {
+        show_hidden_Menu();
+        return;
+    }
+    return QMainWindow::keyPressEvent(event);
+}
+
 void MainWindow::do_key_event(Mcu::KeyType type)
 {
     switch (type) {
@@ -119,15 +135,6 @@ void MainWindow::update_translator(QString string)
 //    firstSecondMenu->retranslate_main_menu_ui(string);
 //    commonMenuWidget->retranslate_common_menu_ui();
 
-}
-
-void MainWindow::keyPressEvent(QKeyEvent *event)
-{
-    if (event->key() == Qt::Key_Alt) {
-        show_hidden_Menu();
-        return;
-    }
-    return QMainWindow::keyPressEvent(event);
 }
 
 void MainWindow::show_hidden_Menu()
