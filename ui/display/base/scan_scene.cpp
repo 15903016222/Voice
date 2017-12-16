194682649aafc066682efa31280e7bf335d57a14
Jake Yang <yanghuanjie@cndoppler.cn>
2017-12-16 13:32:02 +0800

    添加: ScanScene添加drawBackground(const QImage &image)接口

--- ui/display/base/scan_scene.cpp
+++ ui/display/base/scan_scene.cpp
@@ -5,9 +5,24 @@
  * @date 2017-07-13
  */
 #include "scan_scene.h"
-
+#include "QPainter"
 
 ScanScene::ScanScene(QObject *parent) :
     QGraphicsScene(parent)
 {
+    connect(this, SIGNAL(update_requested()),
+            this, SLOT(update()), Qt::QueuedConnection);
+}
+
+void ScanScene::update()
+{
+    QGraphicsScene::update();
+}
+
+void ScanScene::drawBackground(QPainter *painter, const QRectF &rect)
+{
+    if (m_pixmap.isNull()) {
+        return;
+    }
+    painter->drawPixmap(rect, m_pixmap, m_pixmap.rect());
 }
--- ui/display/base/scan_scene.h
+++ ui/display/base/scan_scene.h
@@ -14,7 +14,27 @@ class ScanScene : public QGraphicsScene
     Q_OBJECT
 public:
     explicit ScanScene(QObject *parent = 0);
+
+    void drawBackground(const QImage &image);
+
+signals:
+    void update_requested();
+
+protected slots:
+    void update();
+
+protected:
+    void drawBackground(QPainter *painter, const QRectF &rect);
+
+private:
+    QPixmap m_pixmap;
 };
+
+inline void ScanScene::drawBackground(const QImage &image)
+{
+    m_pixmap = QPixmap::fromImage(image);
+    emit update_requested();
+}
 
 
 #endif // __SCAN_SCENE_H__
