e7eb6bb6396d14b30c5569a92aae4682e086f734
Jake Yang <yanghuanjie@cndoppler.cn>
2017-12-20 18:52:12 +0800

    修改: IndexRuler

--- ui/display/ruler/index_ruler.cpp
+++ ui/display/ruler/index_ruler.cpp
@@ -1,7 +1,19 @@
 #include "index_ruler.h"
 
-IndexRuler::IndexRuler(MarkPostion position, QWidget *parent) :
-    Ruler(position, "(mm)", parent)
+IndexRuler::IndexRuler(const DplDevice::GroupPointer &group, MarkPostion position, QWidget *parent) :
+    Ruler(position, "(mm)", parent),
+    m_group(group)
 {
+    set_background_color(PaleGreenColor);
     set_scroll(true);
+}
+
+void IndexRuler::update()
+{
+    Ruler::update();
+}
+
+void IndexRuler::resizeEvent(QResizeEvent *e)
+{
+    Ruler::resizeEvent(e);
 }
--- ui/display/ruler/index_ruler.h
+++ ui/display/ruler/index_ruler.h
@@ -3,11 +3,22 @@
 
 #include "ruler.h"
 
+#include <device/group.h>
+
 class IndexRuler : public Ruler
 {
     Q_OBJECT
 public:
-    IndexRuler(MarkPostion position, QWidget *parent);
+    IndexRuler(const DplDevice::GroupPointer &group, MarkPostion position, QWidget *parent);
+
+public slots:
+    void update();
+
+protected:
+    void resizeEvent(QResizeEvent *e);
+
+private:
+    DplDevice::GroupPointer m_group;
 };
 
 #endif // __INDEX_RULER_H__
