b5c32a7ef87e988e95137a2639d80f17c1c0370b
Jake Yang <yanghuanjie@cndoppler.cn>
2018-01-15 10:09:06 +0800

    修改: UtRefMenuItem和UtMeasMenuItem改为继承CursorMenuItem

--- ui/menu/measurement/cursors/ut_menu_item.cpp
+++ ui/menu/measurement/cursors/ut_menu_item.cpp
@@ -1,47 +1,14 @@
 #include "ut_menu_item.h"
 
 #include <global.h>
-#include <device/device.h>
+
+#include "../../../tool/tool.h"
 
 UtMenuItem::UtMenuItem(QWidget *parent, const QString &title) :
-    SpinMenuItem(parent, title)
+    CursorMenuItem(parent, title)
 {
     set_decimals(2);
     set_step(0.01);
-    connect(DplDevice::Device::instance(),
-            SIGNAL(current_group_changed(DplDevice::GroupPointer)),
-            this, SLOT(update_group()));
-}
-
-void UtMenuItem::update_group()
-{
-    if (m_group) {
-        disconnect(static_cast<DplDevice::Group *>(m_group.data()),
-                   SIGNAL(ut_unit_changed(DplDevice::Group::UtUnit)),
-                   this, SLOT(update_unit(DplDevice::Group::UtUnit)));
-        disconnect(static_cast<DplUt::Sample *>(m_group->sample().data()),
-                   SIGNAL(start_changed(float)),
-                   this, SLOT(update_range()));
-        disconnect(static_cast<DplUt::Sample *>(m_group->sample().data()),
-                   SIGNAL(range_changed(float)),
-                   this, SLOT(update_range()));
-    }
-
-    m_group = DplDevice::Device::instance()->current_group();
-
-    cursor_change_event(m_group->cursor());
-
-    update_unit(m_group->ut_unit());
-
-    connect(static_cast<DplDevice::Group *>(m_group.data()),
-            SIGNAL(ut_unit_changed(DplDevice::Group::UtUnit)),
-            this, SLOT(update_unit(DplDevice::Group::UtUnit)));
-    connect(static_cast<DplUt::Sample *>(m_group->sample().data()),
-            SIGNAL(start_changed(float)),
-            this, SLOT(update_range()));
-    connect(static_cast<DplUt::Sample *>(m_group->sample().data()),
-            SIGNAL(range_changed(float)),
-            this, SLOT(update_range()));
 }
 
 void UtMenuItem::update_unit(DplDevice::Group::UtUnit type)
@@ -56,12 +23,39 @@ void UtMenuItem::update_unit(DplDevice::Group::UtUnit type)
 
 void UtMenuItem::update_range()
 {
-    set_range(m_group->sample()->start(),
-              m_group->sample()->start() + m_group->sample()->range());
+    set_range(Tool::cnf_to_display(group(), group()->sample()->start()),
+              Tool::cnf_to_display(group(), group()->sample()->start()
+                                   + group()->sample()->range()));
     update_value();
 }
 
-void UtMenuItem::cursor_change_event(const DplMeasure::CursorPointer &cursor)
+void UtMenuItem::disconnect_group(const DplDevice::GroupPointer &group)
 {
-    Q_UNUSED(cursor);
+    disconnect(static_cast<DplDevice::Group *>(group.data()),
+               SIGNAL(ut_unit_changed(DplDevice::Group::UtUnit)),
+               this, SLOT(update_unit(DplDevice::Group::UtUnit)));
+    disconnect(static_cast<DplUt::Sample *>(group->sample().data()),
+               SIGNAL(start_changed(float)),
+               this, SLOT(update_range()));
+    disconnect(static_cast<DplUt::Sample *>(group->sample().data()),
+               SIGNAL(range_changed(float)),
+               this, SLOT(update_range()));
+    CursorMenuItem::disconnect_group(group);
+}
+
+void UtMenuItem::connect_group(const DplDevice::GroupPointer &group)
+{
+    update_unit(group->ut_unit());
+
+    connect(static_cast<DplDevice::Group *>(group.data()),
+            SIGNAL(ut_unit_changed(DplDevice::Group::UtUnit)),
+            this, SLOT(update_unit(DplDevice::Group::UtUnit)));
+    connect(static_cast<DplUt::Sample *>(group->sample().data()),
+            SIGNAL(start_changed(float)),
+            this, SLOT(update_range()));
+    connect(static_cast<DplUt::Sample *>(group->sample().data()),
+            SIGNAL(range_changed(float)),
+            this, SLOT(update_range()));
+
+    CursorMenuItem::connect_group(group);
 }
