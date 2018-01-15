d77b5f2e5e707b72714dc649d8db415a4c913aaa
Jake Yang <yanghuanjie@cndoppler.cn>
2018-01-15 16:19:30 +0800

    添加: CscanDisplay添加扫查参考和测量光标

--- ui/display/c_scan/c_scan_display.h
+++ ui/display/c_scan/c_scan_display.h
@@ -8,6 +8,8 @@
 class ScanRuler;
 class IndexRuler;
 class CscanItem;
+class ScanRefCursorItem;
+class ScanMeasCursorItem;
 
 class CscanDisplay : public ScanDisplay
 {
@@ -16,14 +18,28 @@ public:
     explicit CscanDisplay(const DplDevice::GroupPointer &grp, QWidget *parent = 0);
     ~CscanDisplay();
 
-signals:
-
 public slots:
     void resize_event(const QSize &size);
 
 protected:
+    ScanRefCursorItem * scan_reference_cursor_item() const;
+    ScanMeasCursorItem * scan_measurement_cursor_item() const;
+
+private:
     DplDevice::GroupPointer m_group;
     CscanItem *m_cscanItem;
+    ScanRefCursorItem *m_scanRefCursorItem;
+    ScanMeasCursorItem *m_scanMeasCursorItem;
 };
+
+inline ScanRefCursorItem *CscanDisplay::scan_reference_cursor_item() const
+{
+    return m_scanRefCursorItem;
+}
+
+inline ScanMeasCursorItem *CscanDisplay::scan_measurement_cursor_item() const
+{
+    return m_scanMeasCursorItem;
+}
 
 #endif // __C_SCAN_DISPLAY_H__
--- ui/display/c_scan/c_scan_hdisplay.h
+++ ui/display/c_scan/c_scan_hdisplay.h
@@ -8,9 +8,6 @@ class CscanHDisplay : public CscanDisplay
 public:
     explicit CscanHDisplay(const DplDevice::GroupPointer &grp, QWidget *parent = 0);
 
-protected slots:
-    void resize_event(const QSize &size);
-
 private:
     ScanRuler *m_scanRuler;
     IndexRuler *m_indexRuler;
